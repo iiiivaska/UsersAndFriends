260522186885e7717cd6e5f53fb51b730f58a91e
iiiivaska <iiiivaska@yandex.ru>
2021-11-12 13:39:01 +0300

    Initial Commit

---
+++ UsersAndFriends/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  UsersAndFriends
+//
+//  Created by Василий Буланов on 11/12/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
